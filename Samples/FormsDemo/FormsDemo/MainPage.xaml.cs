70108710307d5738c4898ae170ab0c2c0b2491d6
Stephen Schaub <sschaub@sermonaudio.com>
2022-11-21 14:26:46 -0500

    Add sample project

---
+++ Samples/FormsDemo/FormsDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaManager;
+using Xamarin.Forms;
+
+namespace FormsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PlayClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossMediaManager.Current.Play("https://samplelib.com/lib/preview/mp3/sample-3s.mp3");
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
